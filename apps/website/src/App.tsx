4f8a09bfd02382910f1c86f6c736282b6e207960
Darin Webb <darin@code.org>
2024-10-09 14:26:28 -0500

    initial commit after dev env deployment

---
+++ apps/website/src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Website } from "@webiny/app-website";
+import "./App.scss";
+
+export const App = () => {
+    return <Website />;
+};
---
+++ webiny.project.ts
@@ -0,0 +1,37 @@
+import cliWorkspaces from "@webiny/cli-plugin-workspaces";
+import cliPulumiDeploy from "@webiny/cli-plugin-deploy-pulumi";
+import cliAwsTemplate from "@webiny/cwp-template-aws/cli";
+
+// Scaffolds.
+import cliScaffold from "@webiny/cli-plugin-scaffold";
+import cliScaffoldExtendGraphQlApi from "@webiny/cli-plugin-scaffold-graphql-service";
+import cliScaffoldAdminModule from "@webiny/cli-plugin-scaffold-admin-app-module";
+import cliScaffoldExtensions from "@webiny/cli-plugin-scaffold-extensions";
+import cliScaffoldWorkspaces from "@webiny/cli-plugin-scaffold-workspaces";
+import cliScaffoldCiCd from "@webiny/cli-plugin-scaffold-ci";
+
+export default {
+    template: "@webiny/cwp-template-aws@5.40.6",
+    name: "cdo-webiny",
+    cli: {
+        plugins: [
+            cliWorkspaces(),
+            cliPulumiDeploy(),
+            cliAwsTemplate(),
+
+            // Scaffolds.
+            cliScaffold(),
+            cliScaffoldExtendGraphQlApi(),
+            cliScaffoldAdminModule(),
+            cliScaffoldExtensions(),
+            cliScaffoldWorkspaces(),
+            cliScaffoldCiCd()
+        ]
+    },
+    appAliases: {
+        core: "apps/core",
+        api: "apps/api",
+        admin: "apps/admin",
+        website: "apps/website"
+    }
+};
